755a18c473b7aced3a26ae99ffe22dc417ea743a
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2022-02-18 15:54:57 +0100

    Updated submodule

--- bot.lua
+++ bot.lua
@@ -9,10 +9,10 @@ require "auth"
 
 local twitch = require "twitch.twitch"
 
--- Connect to Twitch server
+-- Connects to Twitch server
 local client = twitch.connect(_USERNAME, _TOKEN_AUTH)
 
--- Create a echo command
+-- Creates an "echo" command
 local function echo(client, channel, username, ...)
     local msg = ""
     for _, value in ipairs({...}) do
@@ -22,16 +22,16 @@ local function echo(client, channel, username, ...)
     client:send(channel, string.format("@%s %s", username, msg))
 end
 
--- Join to our channel
+-- Joins to our channel
 client:join(_CHANNEL)
--- Send a message in our channel
+-- Sends a message in our channel
 client:send(_CHANNEL, "Hello world!")
--- Add a command
+-- Adds a command
 client:attach("echo", _CHANNEL, echo)
--- Add a not found command
+-- Adds a not found command
 function client:commandnotfound(channel, username, command)
     self:send(channel, string.format("Command !%s not found", command))
 end
 
--- Run commands
+-- Runs commands
 client:loop()
